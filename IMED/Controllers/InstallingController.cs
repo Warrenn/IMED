53a554ee76343acc47a3736b890e1edbfa35c506
ShakaZ <ShakaZ@DESKTOP-U6ELTCD>
2016-09-07 14:27:41 +0100

    Injecting Services, DashboardService and InstallingService

--- IMED/Controllers/InstallingController.cs
+++ IMED/Controllers/InstallingController.cs
@@ -2,6 +2,7 @@
 using Fare;
 using FizzWare.NBuilder;
 using IMED.Models;
+using IMED.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,79 +14,27 @@ namespace IMED.Controllers
 {
     public class InstallingController : ApiController
     {
+        private IInstallingService service;
 
-        static readonly IEnumerable<InstallingPayRollSupportInProgress> payRollSupportProgress;
-
-        static readonly IEnumerable<InstallingSchemeInstalled> SchemedInstalled;
-
-        static readonly Xeger QuoteNumberGenerator = new Xeger("R(\\d{6})S", new Random((int)DateTime.Now.Ticks));
-
-        static readonly Xeger SchemeNumberGenerator = new Xeger("R(\\d{10})S", new Random((int)DateTime.Now.Ticks));
-
-        static readonly RandomGenerator RandomGeneratorGenerator = new RandomGenerator();
-
-        static InstallingController()
+        public InstallingController():this(new InstallingService())
         {
-            //New Business Installing  Pay Roll Suport In Progress fake data
-            payRollSupportProgress = Builder<InstallingPayRollSupportInProgress>
-               .CreateListOfSize(100)
-               .All()
-               .With(q => q.ClientName = Name.FullName(NameFormats.Standard))
-               .With(q => q.QuoteNumber = QuoteNumberGenerator.Generate())
-               .With(q => q.SchemeName = Name.FullName(NameFormats.Standard))
-               .With(q=> q.SchemeNumber = SchemeNumberGenerator.Generate())
-               .With(q=> q.PayPointNamer = Name.FullName(NameFormats.Standard)).Build();
 
+        }
 
-            // New Business Installing Scheme Installed  fake data
-
-            SchemedInstalled = Builder<InstallingSchemeInstalled>
-             .CreateListOfSize(100)
-             .All()
-             .With(q => q.ClientName = Name.FullName(NameFormats.Standard))
-             .With(q => q.QuoteNumber = QuoteNumberGenerator.Generate())
-             .With(q => q.SchemeNumber = SchemeNumberGenerator.Generate())
-             .With(q => q.SchemeName = Name.FullName(NameFormats.Standard))
-             .Build();
+        public InstallingController(IInstallingService service)
+        {
+            this.service = service;
         }
 
         [HttpGet]
         public PagedResult<InstallingPayRollSupportInProgress> GetPayRollSupportInProgress([FromUri]PagedRequest<string> request)
         {
-
-            var allMatches = payRollSupportProgress
-                .Where(q =>
-                string.IsNullOrEmpty(request.Filter) ||
-                q.ClientName.Contains(request.Filter) ||
-                q.QuoteNumber.Contains(request.Filter));
-            var count = allMatches.Count();
-            var data = allMatches.Skip(request.Skip).Take(request.Take);
-
-            return new PagedResult<InstallingPayRollSupportInProgress>
-            {
-                Count = count,
-                Data = data
-            };
+            return service.GetPayRollSupportInProgress(request);
         }
         [HttpGet]
         public PagedResult<InstallingSchemeInstalled> GetSchemeInstalled([FromUri]PagedRequest<string> request)
         {
-
-            var allMatches = SchemedInstalled
-                .Where(q =>
-                string.IsNullOrEmpty(request.Filter) ||
-                q.ClientName.Contains(request.Filter) ||
-                q.QuoteNumber.Contains(request.Filter));
-            var count = allMatches.Count();
-            var data = allMatches.Skip(request.Skip).Take(request.Take);
-
-            return new PagedResult<InstallingSchemeInstalled>
-            {
-                Count = count,
-                Data = data
-            };
+            return service.GetSchemeInstalled(request);
         }
-
-
     }
 }
---
+++ IMED/Services/IInstallingService.cs
@@ -0,0 +1,15 @@
+﻿using IMED.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMED.Services
+{
+    public interface IInstallingService
+    {
+        PagedResult<InstallingPayRollSupportInProgress> GetPayRollSupportInProgress(PagedRequest<string> request);
+        PagedResult<InstallingSchemeInstalled> GetSchemeInstalled(PagedRequest<string> request);
+    }
+}
