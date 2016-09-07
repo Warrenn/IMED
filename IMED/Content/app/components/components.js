caf03254bb63e99d6d3d50cf2e49bf3215ff4676
Warrenn <warrenn@busyweb.co.za>
2016-09-07 14:23:03 +0200

    updated components index js

--- IMED/Content/app/components/components.js
+++ IMED/Content/app/components/components.js
@@ -1,16 +1,17 @@
 import angular from 'angular';
 import Dashboard from './dashboard/dashboard';
 import QuotesIssued from './quotesIssued/quotesIssued';
-import launching from './launching/launching';
-import NewBusinessInstalling from './installing/installing';
+import Launching from './launching/launching';
+import Installing from './installing/installing';
+import Payroll from './payroll/payroll';
 
 let componentModule = angular
     .module('app.components', [
         Dashboard,
         QuotesIssued,
-        launching,
-        NewBusinessInstalling
-
+        Launching,
+        Installing,
+        Payroll
     ])
     .name;
 
