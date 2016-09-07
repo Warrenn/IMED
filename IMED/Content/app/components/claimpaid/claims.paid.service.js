4db087627b40c4ed251c37250146fca484bc4518
siyamcela <siyamcelamadokwe@gmail.com>
2016-09-07 14:33:42 +0200

    added claims paid

---
+++ IMED/Content/app/components/claimpaid/claims.paid.service.js
@@ -0,0 +1,20 @@
+﻿class ClaimsPaidService {
+	constructor($http, $httpParamSerializer) {
+		'ngInject';
+
+		this.getPaidClaims = (request) => {
+			const  options = {
+				url: '/api/ClaimsPaid/GetPaidClaims?' + $httpParamSerializer(request),
+				method: 'GET'
+			}
+			return $http(options).then((response) => {
+				return {
+					response,
+					request
+				}
+			});
+		}
+	}
+}
+
+export default ClaimsPaidService;
--- IMED/Content/app/components/components.js
+++ IMED/Content/app/components/components.js
@@ -2,12 +2,15 @@ import angular from 'angular';
 import Dashboard from './dashboard/dashboard';
 import QuotesIssued from './quotesIssued/quotesIssued';
 import Payroll from './payroll/payroll';
+import ClaimPaid from './claimpaid/claimpaid';
+
 
 let componentModule = angular
     .module('app.components', [
         Dashboard,
         QuotesIssued,
-        Payroll
+        Payroll,
+        ClaimPaid
     ])
     .name;
 
