487b87fef15614154c4d4f403213d385357e5838
ShakaZ <ShakaZ@DESKTOP-U6ELTCD>
2016-09-08 09:14:56 +0100

    LaunchingController refractor and LaunchingService interface and implementation

--- IMED/Controllers/LaunchingController.cs
+++ IMED/Controllers/LaunchingController.cs
@@ -2,6 +2,7 @@
 using Fare;
 using FizzWare.NBuilder;
 using IMED.Models;
+using IMED.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,76 +14,28 @@ namespace IMED.Controllers
 {
     public class LaunchingController : ApiController
     {
-        static readonly IEnumerable<LaunchingCompleted> LaunchingCompleted;
+        private ILynchingService service;
 
-        static readonly IEnumerable<LaunchingIncomplete> LaunchingInComplete;
-
-        static readonly Xeger QuoteNumberGenerator = new Xeger("R(\\d{6})S", new Random((int)DateTime.Now.Ticks));
-
-        static readonly RandomGenerator RandomGeneratorGenerator = new RandomGenerator();
-
-        static LaunchingController()
+        public LaunchingController():this(new LynchingService())
         {
-
-            //launching complete fake data
-            LaunchingCompleted = Builder<LaunchingCompleted>
-               .CreateListOfSize(100)
-               .All()
-               .With(q => q.ClientName = Name.FullName(NameFormats.Standard))
-               .With(q => q.QuoteNumber = QuoteNumberGenerator.Generate()).Build();
-
-
-            // launching Incompleted fake data
-
-            LaunchingInComplete = Builder<LaunchingIncomplete>
-             .CreateListOfSize(100)
-             .All()
-             .With(q => q.ClientName = Name.FullName(NameFormats.Standard))
-             .With(q => q.QuoteNumber = QuoteNumberGenerator.Generate())
-             .With(q=> q.Comments = Lorem.Paragraph(10))
-             .Build();
 
         }
 
+        public LaunchingController(ILynchingService service)
+        {
+            this.service = service;
+        }
 
         [HttpGet]
         public PagedResult<LaunchingCompleted> Completed([FromUri]PagedRequest<string> request)
         {
-
-            var allMatches = LaunchingCompleted
-                .Where(q =>
-                string.IsNullOrEmpty(request.Filter) ||
-                q.ClientName.Contains(request.Filter) ||
-                q.QuoteNumber.Contains(request.Filter));
-            var count = allMatches.Count();
-            var data = allMatches.Skip(request.Skip).Take(request.Take);
-
-            return new PagedResult<LaunchingCompleted>
-            {
-                Count = count,
-                Data = data
-            };
+            return service.Completed(request);
         }
 
         [HttpGet]
         public PagedResult<LaunchingIncomplete> Incomplete([FromUri]PagedRequest<string> request)
         {
-
-            var allMatches = LaunchingInComplete
-                .Where(q =>
-                string.IsNullOrEmpty(request.Filter) ||
-                q.ClientName.Contains(request.Filter) ||
-                q.QuoteNumber.Contains(request.Filter));
-            var count = allMatches.Count();
-            var data = allMatches.Skip(request.Skip).Take(request.Take);
-
-            return new PagedResult<LaunchingIncomplete>
-            {
-                Count = count,
-                Data = data
-            };
+            return service.Incomplete(request);
         }
-
-
     }
 }
